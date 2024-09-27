835e446d1b6f0898920b96acccb51dd71cca30df
PC <aluisaybar@gmail.com>
2024-09-27 17:11:50 -0500

    update

--- app_3.py
+++ app_3.py
@@ -14,7 +14,7 @@ from datetime import datetime
 import pytz
 
 # Configurar la base de datos
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:lgGiHKEKyIifdAnSznrKUeuKbvGUgrqs@junction.proxy.rlwy.net:44091/railway"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:yfVsMyfoDbTqzoXXUutIjZBRsuaKRaRW@autorack.proxy.rlwy.net:11691/railway"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 metadata = MetaData()
  
